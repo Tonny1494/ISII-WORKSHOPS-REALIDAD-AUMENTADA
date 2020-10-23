a22637d7b95d82eb3d68ea8531ef24b4513d5ea7
Tonny1494 <tonny_espinoza1494@hotmail.com>
2020-10-22 21:11:02 -0500

    add attribute

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -1,5 +1,17 @@
 package ec.edu.espol.workshops.second;
 
 public class CarInsurance {
+	public char sexo;
+	public int  Age ;
+	public boolean marital;
+	
+	public CarInsurance(char sexo, int age, boolean marital) {
+		super();
+		this.sexo = sexo;
+		Age = age;
+		this.marital = marital;
+	}
 
+	
+	
 }
