4a876ce42ee2bd3ac5245c58e2bd6fd4cbb375dc
eileenbelen <usuario@DESKTOP-BL6QRGO>
2020-10-23 10:28:12 -0500

    Premium CarInsurance - Tester

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -35,6 +35,28 @@ public class CarInsurance {
 	public void setMarital(boolean marital) {
 		this.marital = marital;
 	}
+	
+	public int getPremium() {
+		int base = 500;
+		if (this.sexo=='M' && this.marital==false && this.Age<=25) {
+			return base + 1500;
+			
+		}
+		if (this.sexo=='F' || this.marital==false) {
+			return base - 200;
+		}
+		if (this.Age>=45 && this.Age<65) {
+			return base -100;
+			
+		}
+		
+	}
+	
+	
+	public static void main(String [] args) {
+		CarInsurance user1 = new CarInsurance("M",23,false);
+		System.out.println(user1.getPremium())
+	}
 
 	
 	
