2754e4baa1121879d1eed809e656f84078346573
Tonny1494 <tonny_espinoza1494@hotmail.com>
2020-10-30 11:02:33 -0500

    resolve problem SpotBugs

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -1,66 +1,59 @@
 package ec.edu.espol.workshops.second;
 public class CarInsurance {
-	public char sexo;
-	public int  age ;
-	public boolean marital;
-	public CarInsurance(char sexo, int age, boolean marital) {
-		super();
-		this.sexo=sexo;
-		age=age;
-		this.marital =marital;
-	}
+   public char sexo;
+   public int  age;
+   public boolean marital;
+   public CarInsurance(char sexo, int age, boolean marital) {
+    super();
+	this.sexo=sexo;
+	this.age=age;
+	this.marital =marital;
+}
+  public CarInsurance(String string, int age2, boolean marital2) {
+   //TODO Auto-generated constructor stub
+  }
 
-	public CarInsurance(String string, int age2, boolean marital2) {
-		// TODO Auto-generated constructor stub
-	}
+  public char getSexo() {
+    return sexo;
+  }
 
-	public char getSexo() {
-		return sexo;
-	}
+  public void setSexo(char sexo) {
+    this.sexo = sexo;
+  }
 
-	public void setSexo(char sexo) {
-		this.sexo = sexo;
-	}
+  public int getAge() {
+    return age;
+  }
 
-	public int getAge() {
-		return age;
-	}
+  public void setAge(int age) {
+    this.age=age;
+  }
 
-	public void setAge(int age) {
-		age=age;
-	}
+  public boolean isMarital() {
+    return marital;
+  }
 
-	public boolean isMarital() {
-		return marital;
-	}
-
-	public void setMarital(boolean marital) {
-		this.marital=marital;
-	}
+  public void setMarital(boolean marital) {
+    this.marital=marital;
+  }
 	
-	public int getPremium() {
-		int base=500;
-		if (this.sexo=='M' && this.marital==false && this.age<=25) {
-			return base + 1500;
+  public int getPremium() {
+    int base=500;
+	if (this.sexo=='M' && this.marital==false && this.age<=25) {
+      return base + 1500;
+  }
+  if (this.sexo=='F' || this.marital==false) {
+    return base-200;
+  }
+  if (this.age>=45 && this.age<65) {
+    return base-100;
 			
-		}
-		if (this.sexo=='F' || this.marital==false) {
-			return base-200;
-		}
-		if (this.age>=45 && this.age<65) {
-			return base-100;
-			
-		}
+  }
 	return base;
 		
-	}
-	
-	
-	public static void main(String [] args) {
-		CarInsurance user1 = new CarInsurance("M",23,false);
-		System.out.println(user1.getPremium());
-	}
-
-	
-	
+  }
+  public static void main(String [] args) {
+    CarInsurance user1=new CarInsurance("M" , 23 , false);
+	user1.getPremium();
+  }	
 }
