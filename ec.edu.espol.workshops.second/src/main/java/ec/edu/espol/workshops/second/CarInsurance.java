eb9261571a4aa3cd53e831e489abc16bb6a85ffa
Allison Espinoza <alleespi@espol.edu.ec>
2020-11-09 23:02:19 -0500

    test plan

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -2,13 +2,13 @@ package ec.edu.espol.workshops.second;
 
 public class CarInsurance {
 	public char sexo;
-	public int  Age ;
+	public int  age ;
 	public boolean marital;
 	
 	public CarInsurance(char sexo, int age, boolean marital) {
 		super();
 		this.sexo = sexo;
-		Age = age;
+		this.age = age;
 		this.marital = marital;
 	}
 
@@ -21,11 +21,11 @@ public class CarInsurance {
 	}
 
 	public int getAge() {
-		return Age;
+		return age;
 	}
 
 	public void setAge(int age) {
-		Age = age;
+		this.age = age;
 	}
 
 	public boolean isMarital() {
@@ -38,24 +38,31 @@ public class CarInsurance {
 	
 	public int getPremium() {
 		int base = 500;
-		if (this.sexo=='M' && this.marital==false && this.Age<=25) {
+		if (this.sexo=='M' && this.marital==false && this.age<=25) {
 			return base + 1500;
 			
 		}
 		if (this.sexo=='F' || this.marital==false) {
 			return base - 200;
 		}
-		if (this.Age>=45 && this.Age<65) {
+		if (this.age>=45 && this.age<65) {
 			return base -100;
 			
 		}
+		return base;
 		
 	}
 	
 	
 	public static void main(String [] args) {
-		CarInsurance user1 = new CarInsurance("M",23,false);
-		System.out.println(user1.getPremium())
+		CarInsurance user1 = new CarInsurance('M',23,false);
+		System.out.println(user1.getPremium());
+		CarInsurance user2 = new CarInsurance('F',23,true);
+		System.out.println(user2.getPremium());
+		CarInsurance user3 = new CarInsurance('M',23,true);
+		System.out.println(user3.getPremium());
+		CarInsurance user4 = new CarInsurance('M',45,true);
+		System.out.println(user4.getPremium());
 	}
 
 	
